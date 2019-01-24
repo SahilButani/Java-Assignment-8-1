9d714191860a7bfdb10a9c10ea2eae535a73903b
Sahil Butani <sahilbutani1760@gmail.com>
2019-01-24 11:44:42 +0530

    Update Filematching.java

--- Filematching.java
+++ Filematching.java
@@ -3,23 +3,37 @@ import java.io.*;
 import java.util.Scanner;
 import java.util.regex.*;
 
-public class Filematching
+public class FileMatch
 {
-    public static void main(String a[]) {
-        File directory = new File("/home/zadmin/");
+  /*
+   *This function takes all the files in the "/home/zadmin/" directory and matches the file
+   *with the regular expression given as an input by the user. If the regex matches function
+   *prints the absolute path of the file.
+   */
+  public static fileMatching()
+  {
+    File directory = new File("/home/zadmin/");
         File[] files = directory.listFiles();
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
+        while (sc.hasNext())
+        {
         Pattern p= Pattern.compile(sc.next());
-            for (File f : files) {
-                if (f.isFile()) {
+            for (File f : files)
+            {
+                if (f.isFile())
+                {
                     Matcher m=p.matcher(f.getName());
-                    if(m.matches()){
+                    if(m.matches())
+                    {
                         System.out.println(f.getAbsolutePath());
                     }
                 }
             }
-
         }
+    }
+
+    public static void main(String a[])
+    {
+        fileMatching();
     }
 }
